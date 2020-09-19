790fbed5058a951462cce4d84605e34dec4ee9f7
MakarPustovalov <makar.pht@gmail.com>
2020-09-19 13:05:07 +0300

    offers done

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
   new WOW().init();
+  
+  const anchors = document.querySelectorAll('a[href*="#"]')
 
+  for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault()
+      
+      const blockID = anchor.getAttribute('href').substr(1)
+      
+      document.getElementById(blockID).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    })
+  }
 })
