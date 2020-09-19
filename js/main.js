c75eaeb5868641af87546022675cf949a7dc8307
MakarPustovalov <makar.pht@gmail.com>
2020-09-19 13:44:05 +0300

    portfolio done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   new WOW().init();
-  
+
   const anchors = document.querySelectorAll('a[href*="#"]')
 
   for (let anchor of anchors) {
@@ -15,4 +15,28 @@ $(document).ready(function() {
       })
     })
   }
+
+  //Смена фона в portfolio
+
+  const portfolio1 = document.querySelector("#portfolio1");
+
+  portfolio1.addEventListener("mouseover", function() {
+    $(".portfolio").css("background-color", "rgba(0, 0, 0, 0.8)");
+    $(".portfolio__bg_leti-studio").css("visibility", "visible");
+  });
+
+  portfolio1.addEventListener("mouseout", function() {
+    $(".portfolio").css("background-color", "#1F1F1F");
+    $(".portfolio__bg_leti-studio").css("visibility", "hidden");
+  });
+
+  portfolio2.addEventListener("mouseover", function() {
+    $(".portfolio").css("background-color", "rgba(0, 0, 0, 0.8)");
+    $(".portfolio__bg_lightroom").css("visibility", "visible");
+  });
+
+  portfolio2.addEventListener("mouseout", function() {
+    $(".portfolio").css("background-color", "#1F1F1F");
+    $(".portfolio__bg_lightroom").css("visibility", "hidden");
+  });
 })
